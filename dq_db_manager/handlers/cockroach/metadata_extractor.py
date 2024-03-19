5d2b49863a2a2a25277dca46a20a2dab27e6af8c
gg-dev-05 <garvitgalgat@gmail.com>
2024-03-19 13:11:38 +0530

    removed unused metadata extractor for cockroach

--- dq_db_manager/handlers/cockroach/metadata_extractor.py
+++
@@ -1,6 +0,0 @@
-from dq_db_manager.handlers.base.metadata_extractor import BaseMetadataExtractor
-from dq_db_manager.handlers.postgresql.metadata_extractor import PostgreSQLMetadataExtractor
-
-class CockroachMetadataExtractor(PostgreSQLMetadataExtractor):
-    def __init__(self, connection_handler, models):
-        super().__init__(connection_handler, models)
